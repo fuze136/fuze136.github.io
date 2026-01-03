ecd50d72447b3d24c9ed90bd395c29ab23cd3156
fuze136 <93542091+fuze136@users.noreply.github.com>
2026-01-04 00:07:39 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+let msg1 = "Bienvenid@ a mi web, échale un vistazo a todo. ;)";
+let msg2 = "\nAtención la web está en BETA (puede dar errores o no estar completa).";
+let mensaje_alerta = msg1 + msg2;
+
+alert(mensaje_alerta);
+console.log(mensaje_alerta);
