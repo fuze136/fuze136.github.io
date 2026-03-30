c9d30b6764464860836ece0fca0a367d30ade625
fuze136 <aitormanzano18@gmail.com>
2026-03-30 11:50:04 +0200

    Web_Update30032026

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## fuze136.github.io
 
 Support: minelevelssoporte@gmail.com
+Making a self made hosting with the webpage shortly, go and check my new announcements!
 <br></br>
 <a href="https://discord.com/invite/CpHyg3A4BJ"><img src="https://play-lh.googleusercontent.com/RSKcY3dSvYVqPgabkZ3xmAjzpOBKmgmMZNOxLvxeab3cHmrcFvW_DeIPY863FWA1sOA" height="50"></a>
--- javascript/script.js
+++ javascript/script.js
@@ -2,5 +2,13 @@ let msg1 = "Bienvenid@ a mi web, échale un vistazo a todo. ;)";
 let msg2 = "\nAtención la web está en BETA (puede dar errores o no estar completa).";
 let mensaje_alerta = msg1 + msg2;
 
+let actualizaciones = "NUEVAS ACTUALIZACIONES: \n";
+let contenidonuevo = "Nuevo: Apartado Juegos Wiki \nNuevo TGN Descarga Link \nSe han agregado mejoras de contenido y modos claro/oscuro \nAgregando: CS2, WikiStats, Tienda y Foro.";
+
+let news = actualizaciones + contenidonuevo;
+
 alert(mensaje_alerta);
 console.log(mensaje_alerta);
+
+alert(news);
+console.log(news);
